7825ee04a4fda173da9c2012b8a4d10c978dd985
Yomna Ahmed <yomnaahmed727@gmail.com>
2025-03-11 14:34:43 +0200

    create add notes button to bottom sheet and finish notes bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// import 'package:notes_app/constant.dart';
+import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,7 +10,20 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(children: [SizedBox(height: 32), CustomTextField()]),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32),
+            CustomTextField(hint: 'Title'),
+            SizedBox(height: 16),
+            CustomTextField(hint: 'Content', maxLines: 7),
+            SizedBox(height: 70),
+            CustomButton(),
+            SizedBox(height: 16),
+          ],
+        ),
+      ),
     );
   }
 }
+
